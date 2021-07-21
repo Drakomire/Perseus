7bb63fe6ad8c1a2e9397e0899742128c8d0846aa
Drakomire <Bambolambo0@gmail.com>
2021-07-20 22:59:51 -0400

    Removed Gear portion of API. Changed to ships work with server.

---
+++ tests/ships.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.path.append('../src')
+from perseus import Perseus, Lang
+
+from pprint import pprint
+
+api = Perseus(url="http://perseusapi.duckdns.org:5000")
+
+import time
+
+# for s in api.getAllShips():
+#     slots = s.slot_ids
+#     slots_flattened = [slot for all_in_slot in slots for slot in all_in_slot]
+#     if 12 in slots_flattened:
+#         print(s.name)
+
+s = api.Ship("Warspite")
+print(s.slot_names)
+print(s.rarity)
