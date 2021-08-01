40e955eabfc95977802df457db143569516fe190
Drakomire <Bambolambo0@gmail.com>
2021-08-01 14:01:30 -0400

    gear update

--- tests/ships.py
+++ tests/ships.py
@@ -1,10 +1,12 @@
 import sys
 
 sys.path.append('../src')
-from perseus import Perseus, Lang
+from perseus import Perseus, Lang, Pos
+from perseus._ships.skill import Skill
 
 from pprint import pprint
 
+# api = Perseus(url="http://localhost:5000")
 api = Perseus(url="http://perseusapi.duckdns.org:5000")
 
 import time
@@ -15,7 +17,24 @@ import time
 #     if 12 in slots_flattened:
 #         print(s.name)
 
-s = api.Ship("Saratoga")
-print(s.slot_names)
-print(s.armor_id)
-print(s.armor_type)
+# s = api.Ship("Nagato",nicknames=True)
+# ship2 = api.Ship("Taihou")
+
+# print(f"Nagato effects on Taihou {Skill.apply_skill(ship2,[],s.skills[0],pos=Pos.FLAGSHIP)}")
+# print(f"Nagato effects on self {Skill.apply_skill(s,[],s.skills[0],pos=Pos.FLAGSHIP)}")
+
+# s = api.Ship("Bremerton")
+# print(f"Effects on self (Lead) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.LEADER)}")
+# print(f"Effects on self (Center) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.CENTER)}")
+# print(f"Effects on self (Rear) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.REAR)}")
+
+# s = api.Ship("San Francisco")
+# print(f"Effects on self (Lead) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.LEADER)}")
+# print(f"Effects on self (Center) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.CENTER)}")
+# print(f"Effects on self (Rear) {Skill.apply_skill(s,[],s.skills[1],pos=Pos.REAR)}")
+
+s = api.Ship("Azuma")
+print(s.rarity)
+
+# print(s.skills[0]._game_effect)
+
